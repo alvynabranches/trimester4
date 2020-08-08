e1844610ebdd644264d6660c27867217345a73f1
Alvyn Abranches <alvynabranches@gmail.com>
2020-08-08 16:04:19 +0530

    08082020v1

--- push_to_github.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-m = input('Enter the commit message: ')
-
-repo = 'origin'
-branch = 'master'
-
-subprocess.call(['git', 'add', '.'])
-subprocess.call(['git', 'commit', '-m', m])
-subprocess.call(['git', 'push', repo, branch])
