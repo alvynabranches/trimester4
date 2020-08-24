3a672e8f65a67f9e04438b6cfd8aa12b0b41f27d
Alvyn Abranches <alvynabranches@gmail.com>
2020-08-24 23:52:59 +0530

    24082020v1

---
+++ 6_Open_Elective/assignment_04/a1.m
@@ -0,0 +1,8 @@
+a = 4
+if (a < 5)
+    fprintf('a is less than 5')
+elseif(a > 5)
+    fprintf('a is more than 5')
+else
+    fprintf('a is equal to 5')
+end
---
+++ 6_Open_Elective/assignment_04/a2.m
@@ -0,0 +1,12 @@
+grade = 'C';
+
+switch(grade)
+    case 'A'
+        fprintf("Excellent\n")
+    case 'B'
+        fprintf("Very Good\n")
+    case 'C'
+        fprintf("Good\n")
+    otherwise
+        fprintf("Invalid\n")
+end
